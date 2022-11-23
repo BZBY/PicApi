9249b9ee19d0f72b83c0fcd339255769ad844769
‘BZBY’ <B_Z_B_Y@qq.com>
2022-11-24 00:03:51 +0800

    s

---
+++ getID.py
@@ -0,0 +1,14 @@
+import os
+filePath = r'./'
+# for i,j,k in os.walk(filePath):
+#     print(i,j,k)
+#os.walk可以用于遍历指定文件下所有的子目录、非目录子文件。
+startFile = os.listdir(filePath)
+for i in startFile:
+    if i.find("Pic_") == 0:
+        pics = os.listdir(i+r"/")
+        print(pics)
+        for ii in pics:
+            with open("img.txt",'a',encoding='utf-8') as f:
+                f.write(ii)
+                f.write("\n")
